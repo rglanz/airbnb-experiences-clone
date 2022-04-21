8cae700aff0d338b8e845514c897018e7c9a76b3
Ryan <glanzrm@gmail.com>
2022-04-21 16:09:52 -0500

    Pull card data from file

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import Navbar from './components/Navbar.jsx'
 import MainContent from './components/MainContent.jsx'
 import Card from './components/Card.jsx'
 
+import cardData from './components/cardData'
+
 function App() {
+  const cardElements = cardData.map(item => {
+    return(
+      <Card 
+        img={item.img}
+        rating={item.rating}
+        reviewCount={item.reviewCount}
+        country={item.country}
+        title={item.title}
+        price={item.price}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <Navbar />
       <MainContent />
-      <Card 
-        img='katie-zaferes.png'
-        rating='5.0'
-        reviewCount={6}
-        country='USA'
-        title='Life lessons with Katie Zaferes'
-        price={136}
-      />
+      <section className="card-container">
+        {cardElements}
+      </section>
     </div>
   )
 }
---
+++ src/components/cardData.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    img: "katie-zaferes.png",
+    rating: "5.0",
+    reviewCount: 6,
+    country: "USA",
+    title: "Life lessons with Katie Zaferes",
+    price: 136,
+  },
+  {
+    img: "wedding-photography.png",
+    rating: "5.0",
+    reviewCount: 30,
+    country: "Finland",
+    title: "Learn wedding photography",
+    price: 125,
+  },
+  {
+    img: "mountain-bike.png",
+    rating: "4.8",
+    reviewCount: 2,
+    country: "Germany",
+    title: "Group Mountain Biking",
+    price: 50,
+  },
+]
